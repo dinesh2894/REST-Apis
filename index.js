730cc2f2e3102b329f4e960ef0586d4f052e5f6d
Dinesh Saini <sainidinesh2894@gmail.com>
2024-02-27 22:34:21 +0530

    create new.ejs, design a form

--- index.js
+++ index.js
@@ -14,25 +14,30 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-let posts = [
-    {
-        username: "apnacollege",
-        content: "I love coding"
-    },
-    {
-        username: "dineshsaini",
-        content: "Hard work is import to achieve success"
-    },
-    {
-        username: "neha",
-        content: "I got selected for 1st internship!!"
-    }
-];
+let posts = [{
+    username: "apnacollege", content: "I love coding"
+}, {
+    username: "dineshsaini", content: "Hard work is import to achieve success"
+}, {
+    username: "neha", content: "I got selected for 1st internship!!"
+}];
 
 
 app.get("/posts", (req, res) => {
     // res.send("Server working well!!");
     res.render("index.ejs", {posts});
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    console.log(req.body);
+    let {username, content} = req.body;
+    posts.push({username, content});
+    // res.send("Post request working");
+    res.redirect('/posts');
 });
 
 app.listen(port, () => {
